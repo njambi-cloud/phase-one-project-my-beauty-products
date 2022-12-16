e2a22f479430da9ff6075057916a6b30f548347c
njambi-cloud <christinemacharia64@gmail.com>
2022-12-16 14:35:31 +0300

    Adds the onclick functionality to the products to be able to view the product details

--- js/index.js
+++ js/index.js
@@ -16,7 +16,31 @@ function render () {
                 li1.textContent=product.name
                 li1.style.listStyleType= "none"
                 ul1.appendChild(li1)
+                li1.addEventListener("click", display)
+                function display() {
+                    let diV = document.createElement("div")
+                    li1.appendChild(diV)
+                    let image = document.createElement("img")
+                    image.src= product.image
+                    let headerS= document.createElement("h5")
+                    headerS.textContent= product.name
+                    let desc = document.createElement("p")
+                    desc.textContent= product.description
+                    let prc = document.createElement("p")
+                    prc.textContent= product.price
+                    let lks = document.createElement("p")
+                    lks.textContent= product.likes
+                    diV.appendChild(image)
+                    diV.appendChild(headerS)
+                    diV.appendChild(desc)
+                    diV.appendChild(prc)
+                    diV.appendChild(lks)
+
+
+                }
                 
             })
         })
 }
+let homebtn= document.getElementById("home")
+homebtn.addEventListener("click", ()=> myShop.removeChild(myShop.children[0]) )
